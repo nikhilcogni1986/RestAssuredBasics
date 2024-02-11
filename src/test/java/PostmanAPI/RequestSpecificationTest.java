49c3d8b4a2303a099c09e13120df6d625eb18770
Nikhil <nikhilcogni@gmail.com>
2024-02-11 22:17:25 +0000

    Reused Request specification

--- src/test/java/PostmanAPI/RequestSpecificationTest.java
+++ src/test/java/PostmanAPI/RequestSpecificationTest.java
@@ -1,20 +1,24 @@
 package PostmanAPI;
 
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class RequestSpecificationTest
-{
-    @Test
-    public void validate_status_code()
-    {
-        RequestSpecification requestSpecification = given().
-                baseUri("https://api.postman.com").
-                header("X-Api-key","PMAK-6217db1b99c8737dca29d714-04ba0929f83569bd0d782e0d75ab006772");
+public class RequestSpecificationTest {
+    RequestSpecification requestSpecification;
 
+    @BeforeClass
+    public void initialize() {
+        requestSpecification = given().
+                baseUri("https://api.postman.com").
+                header("X-Api-key", "PMAK-6217db1b99c8737dca29d714-04ba0929f83569bd0d782e0d75ab006772");
+    }
+
+    @Test
+    public void validate_status_code() {
         given().spec(requestSpecification).
                 when().
                 get("/workspaces").
@@ -25,10 +29,6 @@ public class RequestSpecificationTest
 
     @Test
     public void extract_response() {
-        RequestSpecification requestSpecification = given().
-                baseUri("https://api.postman.com").
-                header("X-Api-key","PMAK-6217db1b99c8737dca29d714-04ba0929f83569bd0d782e0d75ab006772");
-
         given().spec(requestSpecification).
                 when().
                 get("/workspaces").
@@ -36,7 +36,7 @@ public class RequestSpecificationTest
                 assertThat().
                 statusCode(200).
                 body("workspaces.name", hasItems("My Workspace", "Team Workspace"),
-                        "workspaces[0].name",is(equalTo("My Workspace")),
+                        "workspaces[0].name", is(equalTo("My Workspace")),
                         "workspaces.size", is(equalTo(3)));
     }
 }
