e650a97a4af8a3153056c0e580645050a5c25ab0
Nikhil <nikhilcogni@gmail.com>
2024-02-17 01:40:59 +0000

    End to End JIRA test

--- src/test/java/Jira/LoginTest.java
+++ src/test/java/Jira/LoginTest.java
@@ -2,7 +2,11 @@ package Jira;
 
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -23,16 +27,17 @@ public class LoginTest
                 .filter(loginSession)
                 .post("/rest/auth/1/session")
                 .then()
-                .log().all()
                 .assertThat().statusCode(200)
                 .extract().response();
         System.out.println(response);
 
-        given()
-                .pathParam("key","10100")
+        String comment = "This is a validation for comments for an issue";
+
+        String commentResponse = given()
+                .pathParam("key","10201")
                 .header("Content-Type","application/json")
                 .body("{\n" +
-                        "    \"body\": \"This is a comment that only administrators can see.\",\n" +
+                        "    \"body\": \""+comment+"\",\n" +
                         "    \"visibility\": {\n" +
                         "        \"type\": \"role\",\n" +
                         "        \"value\": \"Administrators\"\n" +
@@ -43,7 +48,55 @@ public class LoginTest
                 .post("/rest/api/2/issue/{key}/comment")
                 .then()
                 .assertThat()
-                .statusCode(201);
+                .statusCode(201)
+                .extract().response().asString();
 
+        JsonPath js1 = new JsonPath(commentResponse);
+        String commentID = js1.getString("id");
+        System.out.println("ID for added comment -->"+commentID);
+
+        //Add an attachment to an issue
+        given()
+                .header("X-Atlassian-Token","nocheck")
+                .filter(loginSession)
+                .pathParam("key","10201")
+                .header("Content-Type","multipart/form-data")
+                .multiPart("file",new File("10200,SS.txt"))
+                .log().all()
+                .when()
+                .post("/rest/api/2/issue/{key}/attachments")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+
+        //Get the issue details
+        String getIssueResponse = given()
+                .filter(loginSession)
+                .pathParam("key","10201")
+                .queryParam("fields","comment")
+                .when()
+                .get("/rest/api/2/issue/{key}")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200)
+                .extract().response().asString();
+        System.out.println(getIssueResponse);
+
+        JsonPath js2 = new JsonPath(getIssueResponse);
+        int commentCount = js2.getInt("fields.comment.comments.size()");
+
+        for(int i=0; i< commentCount; i++)
+        {
+            String commentIDIssue = js2.get("fields.comment.comments["+i+"]").toString();
+            System.out.println(commentIDIssue);
+
+            if(commentIDIssue.equalsIgnoreCase(commentID))
+            {
+                String actualComment = js2.get("fields.comment.comments["+i+"].body");
+                System.out.println(actualComment);
+                Assert.assertEquals(actualComment,comment);
+            }
+        }
     }
 }
